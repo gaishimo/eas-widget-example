c8a33d3149c8c78fda340eb3f61371549c9aa8bf
gaishimo <gaithimo@gmail.com>
2022-07-18 12:08:16 +0900

    Add react import

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
   return (
@@ -7,14 +8,14 @@ export default function App() {
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
