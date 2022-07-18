703bdb7035cc5c9683c41e2ad6c9c0e029a9cc7a
gaishimo <gaithimo@gmail.com>
2022-07-18 11:40:37 +0900

    Add prettier configs

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: "typescript",
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: "all",
+  singleQuote: false,
+  semi: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+};
