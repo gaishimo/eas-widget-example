7cd2d95d0d79eb6965f1ba0301cfbe417ba3c068
gaishimo <gaithimo@gmail.com>
2022-08-21 15:04:22 +0900

    Change the message on App

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native"
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>EAS Widget Extension Example</Text>
+      <Text>You can add widget on home screen!</Text>
       <StatusBar style="auto" />
     </View>
   )
