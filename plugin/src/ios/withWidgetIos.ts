47ac85a7a593ec936b989b667d8613a0bd9a08c4
Fernando Rojo <frojo@sas.upenn.edu>
2023-04-16 11:49:35 -0400

    Update withWidgetIos.ts

--- plugin/src/ios/withWidgetIos.ts
+++ plugin/src/ios/withWidgetIos.ts
@@ -1,10 +1,12 @@
 import { ConfigPlugin } from "@expo/config-plugins"
 import { withWidgetXCode } from "./withWidgetXCode"
+import { withWidgetEAS } from "./withWidgetEAS"
 
 export const withWidgetIos: ConfigPlugin<WithWidgetProps> = (
   config,
   options,
 ) => {
   config = withWidgetXCode(config, options)
+  config = withWidgetEAS(config, options)
   return config
 }
